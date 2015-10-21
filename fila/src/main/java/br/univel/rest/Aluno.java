4d540b849e9f33f7ba12cdbbdc508022bbfbae1f
Cleverson <cleversonhirt@gmail.com>
2015-10-21 20:17:20 -0200

    commit maroto

--- fila/src/main/java/br/univel/rest/Aluno.java
+++ fila/src/main/java/br/univel/rest/Aluno.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 
 public class Aluno implements Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private long matricula;
 	private String nome;
 	public long getMatricula() {
