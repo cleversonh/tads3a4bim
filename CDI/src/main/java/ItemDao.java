5f30b1db65e116579d8383ce37f76798760bbca6
Cleverson <cleversonhirt@gmail.com>
2015-10-22 21:48:58 -0200

    Commit inicial

---
+++ CDI/src/main/java/ItemDao.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+/**
+ *
+ * @author nbuser
+ */
+public interface ItemDao {
+
+    List<Item> fetchItems();
+
+}
---
+++ CDI/src/main/java/ItemProcessor.java
@@ -0,0 +1,25 @@
+
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+
+/**
+ *
+ * @author nbuser
+ */
+@Named
+@RequestScoped
+public class ItemProcessor {
+
+    @Inject @Demo
+    private ItemDao itemDao;
+
+    public void execute() {
+        List<Item> items = itemDao.fetchItems();
+        for (Item item : items) {
+            System.out.println("Found item " + item);
+        }
+    }
+}
