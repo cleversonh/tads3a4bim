f1aad69ee480cccb5fbfa7483a87f5ca3f55a751
Cleverson <cleversonhirt@gmail.com>
2015-10-22 22:09:34 -0200

    criado webservet

---
+++ CDI/src/main/java/br/univel/rest/AnotherItemDao.java
@@ -0,0 +1,18 @@
+package br.univel.rest;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author nbuser
+ */
+public class AnotherItemDao implements ItemDao {
+
+    @Override
+    public List<Item> fetchItems() {
+        List<Item> results = new ArrayList<Item>();
+        results.add(new Item(99, 9));
+        return results;
+    }
+
+}
---
+++ CDI/src/main/java/br/univel/rest/ItemDao.java
@@ -0,0 +1,12 @@
+package br.univel.rest;
+import java.util.List;
+
+/**
+ *
+ * @author nbuser
+ */
+public interface ItemDao {
+
+    List<Item> fetchItems();
+
+}
