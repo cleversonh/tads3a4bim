d06cf955361d92b07c54798758cc3b9661a7f3f8
Cleverson <cleversonhirt@gmail.com>
2015-10-22 22:05:59 -0200

    criado webservert

--- CDI/src/main/java/br/univel/rest/RestApplication.java
+++ CDI/src/main/java/br/univel/rest/RestApplication.java
@@ -1,9 +1,35 @@
 package br.univel.rest;
 
-import javax.ws.rs.core.Application;
-import javax.ws.rs.ApplicationPath;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-@ApplicationPath("/rest")
-public class RestApplication extends Application
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+@WebServlet("/restApplication")
+public class RestApplication extends HttpServlet
 {
+	@Inject
+	ItemProcessor ip;
+	
+	
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		try (PrintWriter pw = response.getWriter()){
+			
+			
+			ip.execute();
+			
+			pw.print("enviado");
+			pw.flush();
+		}
+		
+	}
+	
 }
