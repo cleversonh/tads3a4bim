8f52f6b38796e6e2931dba7b84a6f029343aafb8
Cleverson <cleversonhirt@gmail.com>
2015-10-22 22:20:17 -0200

    @Qualifier

--- CDI/src/main/java/br/univel/rest/AnotherItemDao.java
+++ CDI/src/main/java/br/univel/rest/AnotherItemDao.java
@@ -6,13 +6,16 @@ import java.util.List;
  *
  * @author nbuser
  */
+@Demo
 public class AnotherItemDao implements ItemDao {
 
+	
     @Override
     public List<Item> fetchItems() {
         List<Item> results = new ArrayList<Item>();
         results.add(new Item(99, 9));
-        return results;
+        return results; 
     }
+    
 
 }
